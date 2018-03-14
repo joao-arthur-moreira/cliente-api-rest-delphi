8ef9786697d08bd0746ff4c285edb0750a46dcd0
drlingerie <ti3@drling.com.br>
2018-03-14 12:52:27 -0300

    Versionando

---
+++ uPrincipal.dfm
@@ -0,0 +1,55 @@
+object frmPrincipal: TfrmPrincipal
+  Left = 0
+  Top = 0
+  Caption = 'Consulta API Rest DR-Industrial'
+  ClientHeight = 243
+  ClientWidth = 527
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object DBGrid1: TDBGrid
+    Left = 24
+    Top = 64
+    Width = 473
+    Height = 171
+    DataSource = DataSource
+    TabOrder = 0
+    TitleFont.Charset = DEFAULT_CHARSET
+    TitleFont.Color = clWindowText
+    TitleFont.Height = -11
+    TitleFont.Name = 'Tahoma'
+    TitleFont.Style = []
+  end
+  object BitBtn1: TBitBtn
+    Left = 24
+    Top = 16
+    Width = 121
+    Height = 25
+    Caption = 'Consultar API'
+    TabOrder = 1
+    OnClick = BitBtn1Click
+  end
+  object ClientDataSet: TClientDataSet
+    Aggregates = <>
+    Params = <>
+    Left = 256
+    Top = 128
+  end
+  object DataSource: TDataSource
+    DataSet = ClientDataSet
+    Left = 312
+    Top = 128
+  end
+  object HTTPBasicAuthenticator: THTTPBasicAuthenticator
+    Username = 'ti3'
+    Password = 'adm1981'
+    Left = 320
+    Top = 8
+  end
+end
---
+++ uPrincipal.pas
@@ -0,0 +1,138 @@
+unit uPrincipal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, System.JSON, REST.Client, IPPeerClient, REST.Types, Datasnap.DBClient,
+  Data.DB, Vcl.StdCtrls, Vcl.Buttons, Vcl.Grids, Vcl.DBGrids,
+  Data.Bind.Components, Data.Bind.ObjectScope, REST.Authenticator.Basic;
+
+type
+  TfrmPrincipal = class(TForm)
+    DBGrid1: TDBGrid;
+    BitBtn1: TBitBtn;
+    ClientDataSet: TClientDataSet;
+    DataSource: TDataSource;
+    HTTPBasicAuthenticator: THTTPBasicAuthenticator;
+    procedure BitBtn1Click(Sender: TObject);
+  private
+    { Private declarations }
+
+    // Objetos necessário para a comunicação com a API
+    FRESTClient: TRESTClient;
+    FRESTRequest: TRESTRequest;
+    FRESTResponse: TRESTResponse;
+
+    // Objeto para receber o JSON de retorno
+    FJSON: TJSONObject;
+
+    procedure InicializarObjetos;
+    procedure EnviarRequisicao;
+    procedure LiberarObjetos;
+    function ConsultarDadosApiDrIndustrial: olevariant;
+    function ProcessarRetorno: olevariant;
+  public
+    { Public declarations }
+  end;
+
+var
+  frmPrincipal: TfrmPrincipal;
+
+implementation
+
+{$R *.dfm}
+
+{ TForm1 }
+
+procedure TfrmPrincipal.BitBtn1Click(Sender: TObject);
+begin
+  ClientDataSet.Data := ConsultarDadosApiDrIndustrial;
+end;
+
+function TfrmPrincipal.ConsultarDadosApiDrIndustrial: olevariant;
+begin
+  InicializarObjetos;
+  EnviarRequisicao;
+  result := ProcessarRetorno;
+  LiberarObjetos;
+end;
+
+procedure TfrmPrincipal.EnviarRequisicao;
+begin
+  // Configura os parâmetros da requisição
+  // aqui pratim eu digo qual recurso eu quero consumir da minha API Rest, no caso ai o recurso 'coletores'
+  FRESTRequest.Resource := 'coletores';
+
+  // Executa a requisição
+  FRESTRequest.Execute;
+
+  FRESTRequest.GetFullRequestURL();
+
+  // Recebe o retorno em JSON e o atribui ao objeto FJSON
+  FJSON.Parse(TEncoding.ASCII.GetBytes(FRESTResponse.JSONValue.ToString), 0);
+end;
+
+procedure TfrmPrincipal.InicializarObjetos;
+begin
+  // Cria o objeto da classe TRESTClient
+  // aqui pratim é a URL principal da API Rest
+  FRESTClient := TRESTClient.Create('192.168.0.154:8081/');
+
+  // Pratim aqui é um tipo de autorização basic do HTTP tem outros tipos de autenticação como o JWT que é baseado em tokens, depois estuda sobre "oauth2"
+  // Tem que ver se a API que tu vai consumir precisar de autenticação se não precisar não precisa dessa linha
+  FRESTClient.Authenticator := HTTPBasicAuthenticator;
+
+  // Cria o objeto da classe TRESTResponse
+  FRESTResponse := TRESTResponse.Create(nil);
+
+  // Cria e configura o objeto da classe TRESTRequest
+  FRESTRequest := TRESTRequest.Create(nil);
+  FRESTRequest.Client := FRESTClient;
+  FRESTRequest.Response := FRESTResponse;
+  // aqui prato é o método que vai executar POST, GET, PUT, DELETE no caso ele vai usar o GET
+  // estuda sobre os verbos HTTP
+  FRESTRequest.Method := rmGET;
+
+  // Cria o objeto para manipular o JSON
+  FJSON := TJSONObject.Create;
+end;
+
+procedure TfrmPrincipal.LiberarObjetos;
+begin
+  FRESTRequest.Free;
+  FRESTResponse.Free;
+  FRESTClient.Free;
+end;
+
+function TfrmPrincipal.ProcessarRetorno: olevariant;
+var
+  DataSetRetorno: TClientDataSet;
+  JSONValue: TJSONValue;
+  JSONObject: TJSONObject;
+begin
+  DataSetRetorno := TClientDataSet.Create(nil);
+  try
+    // Define as colunas
+    DataSetRetorno.FieldDefs.Add('codigo', Data.DB.ftInteger);
+    DataSetRetorno.FieldDefs.Add('nome', Data.DB.ftString, 40);
+    DataSetRetorno.CreateDataSet;
+
+    // Percorre o JSON, lendo os valores das chaves
+    for JSONValue in FRESTResponse.JSONValue as TJSONArray do
+    begin
+      JSONObject := JSONValue as TJSONObject;
+
+      DataSetRetorno.AppendRecord([
+        JSONObject.GetValue('codigo').Value,
+        JSONObject.GetValue('nome').Value
+      ]);
+    end;
+
+    result := DataSetRetorno.Data;
+  finally
+    DataSetRetorno.Free;
+  end;
+end;
+
+end.
